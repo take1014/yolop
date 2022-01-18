00b48b753a9604a151cb5061bc3135b5785d4b5e
take1014 <y.takehara1014@gmail.com>
2022-01-18 22:48:30 +0900

    add L2Norm, DBox class

--- ssd/model.py
+++ ssd/model.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
+import torch
+from itertools import product
+from math import sqrt
 from torch import nn
 
 def make_vgg():
@@ -42,6 +45,70 @@ def make_extras():
 
     return nn.ModuleList(layers)
 
+def make_loc_conf(num_classes=21, bbox_aspect_num=[4,6,6,6,4,4]):
+    loc_layers  = []
+    conf_layers = []
+
+    in_channels_list = [512, 1024, 512, 256, 256, 256]
+
+    for ch, bbox_num in zip(in_channels_list, bbox_aspect_num):
+        loc_layers  += [nn.Conv2d(ch, bbox_num * 4, kernel_size=3, padding=1)]
+        conf_layers += [nn.Conv2d(ch, bbox_num * num_classes, kernel_size=3, padding=1)]
+
+    return nn.ModuleList(loc_layers), nn.ModuleList(conf_layers)
+
+class L2Norm(nn.Module):
+    def __init__(self, input_channels=512, scale=20):
+        super(L2Norm, self).__init__()
+        self.weight = nn.Parameter(torch.Tensor(input_channels))
+        self.scale = scale
+        self.reset_parameters()
+        self.eps = 1e-10
+
+    def reset_parameters(self):
+        nn.init.constant_(self.weight, self.scale)
+
+    def forward(self, x):
+        norm = x.pow(2).sum(dim=1, keepdim=True).sqrt() + self.eps
+        x = torch.div(x, norm)
+        print(self.weight.shape)
+        print(self.weight)
+        weights = self.weight.unsqueeze(0).unsqueeze(2).unsqueeze(3).expand_as(x)
+        print(weights.shape)
+        print(weights)
+        out = weights * x
+        return out
+
+class DBox(object):
+    def __init__(self, cfg):
+        super(DBox, self).__init__()
+        self.image_size = cfg['input_size']
+        self.feature_maps = cfg['feature_maps']
+        self.steps = cfg['steps']
+        self.min_sizes = cfg['min_sizes']
+        self.max_sizes = cfg['max_sizes']
+        self.aspect_ratios = cfg['aspect_ratios']
+
+    def make_dbox_list(self):
+        mean = []
+        for k, f in enumerate(self.feature_maps):
+            s_k = self.min_sizes[k]/self.image_size
+            f_k = self.image_size / self.steps[k]
+            for i, j in product(range(f), repeat=2):
+                cx = (j + 0.5) / f_k
+                cy = (i + 0.5) / f_k
+
+                mean += [cx, cy, s_k, s_k]
+
+                s_k_prime = sqrt(s_k * (self.max_sizes[k]/self.image_size))
+                mean += [cx, cy, s_k_prime, s_k_prime]
+
+                for ar in self.aspect_ratios[k]:
+                    mean += [cx, cy, s_k * sqrt(ar), s_k/sqrt(ar)]
+                    mean += [cx, cy, s_k / sqrt(ar), s_k*sqrt(ar)]
+        output = torch.Tensor(mean).view(-1, 4)
+        output.clamp_(max=1, min=0)
+        return output
 
 if __name__ == "__main__":
     vgg_test = make_vgg()
@@ -49,3 +116,29 @@ if __name__ == "__main__":
 
     extras_test = make_extras()
     print(extras_test)
+
+    loc_test, conf_test = make_loc_conf()
+    print(loc_test)
+    print(conf_test)
+
+    l2norm = L2Norm()
+    x = torch.randn((4, 512, 38, 38))
+    x = l2norm(x)
+    #print(x)
+    #print(l2norm(x))
+    import pandas as pd
+    ssd_cfg = {
+        'num_classes': 21,
+        'input_size': 300,
+        'bbox_aspect_num': [4, 6, 6, 6, 4, 4],
+        'feature_maps': [38, 19, 10, 5, 3, 1],
+        'steps': [8, 16, 32, 64, 100, 300],
+        'min_sizes': [30, 60, 111, 152, 213, 264],
+        'max_sizes': [60, 111, 162, 213, 264, 315],
+        'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]]
+    }
+
+    dbox = DBox(ssd_cfg)
+    dbox_list = dbox.make_dbox_list()
+    print(pd.DataFrame(dbox_list.numpy()))
+
